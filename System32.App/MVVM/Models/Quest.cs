6ed4666fd69ee69641ee2ab8d8c8878ca0c7095e
IvandryPage <Ivandry.89@gmail.com>
2024-07-21 16:11:09 +0700

    Create Quest and User models

---
+++ System32.App/MVVM/Models/Quest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System32.App.MVVM.Models
+{
+    public class Quest
+    {
+        // Quest pastinya punya judul, deskripsi, sub-task yang harus dikerjakan satu-satu, reward penambahan status user, 
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<string> SubQuest { get; set; } // Perlu ekspansi ke model baru? sebuah subQuest memiliki target dan juga progress yang telah dilakukan user? atau User memiliki List<int> QuestProgresses?
+        public Dictionary<string, int> StatusReward { get; set; }
+    }
+}
---
+++ System32.App/MVVM/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System32.App.MVVM.Models
+{
+    public class User
+    {
+        // User punya nama, punya id, punya data status yang bisa dikembangkan seperti kesehatan, mental/charisma, dan juga kecerdasan
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Intelligence { get; set; }
+        public int Charisma { get; set; }
+    }
+}
